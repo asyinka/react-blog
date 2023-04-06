0fc85e8e5e82c908490d0d099a9108f303dbf8e6
Yinka <asyinka@gmail.com>
2023-04-06 18:23:42 +0100

    add variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 
 function App() {
+  let myTitle = `This is my first React App project`;
+  let likes = 50;
+  let myArr = ["CSS, ", "HTML, ", "JavaScript, ", "React..."];
+
+  //Reac doesnt accept Bolean and objects as variables
+
   return (
     <div className="App">
       <div className="content">
-        <h1>My React App component</h1>
+        <h1> {myTitle} </h1>
+        <p> This project has gotten {likes} likes</p>
+        <p>In the bag are {myArr} and now React!!! loading</p>
       </div>
     </div>
   );
