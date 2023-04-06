34634e1deca087e07de37e3b6634b9c58bda50bb
Yinka <asyinka@gmail.com>
2023-04-06 19:00:48 +0100

    add NavBar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import "./App.css";
+import NavBar from "./NavBar";
 
 function App() {
-  let myTitle = `This is my first React App project`;
-  let likes = 50;
-  let myArr = ["CSS, ", "HTML, ", "JavaScript, ", "React..."];
-  const ggLink = "https://www.google.com";
-
-  //Reac doesnt accept Bolean and objects as variables
-
   return (
     <div className="App">
+      <NavBar></NavBar>
       <div className="content">
-        <h1> {myTitle} </h1>
-        <p> This project has gotten {likes} likes</p>
-        <p>In the bag are {myArr} and now React!!! loading</p>
-        <p>
-          This is {1} Math evaluation {Math.random() * 10} in JSX{" "}
-        </p>
-        <a href={ggLink} target="_blank" rel="noreferrer">
-          Google Site &#10084;
-        </a>
-        {/* //when adding link as variable remove the apostrophes */}
+        <h1> First React project </h1>
       </div>
     </div>
   );
---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <h1>Yinks 'React' Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
