8a97dd200d4795960222437ed448ea627a697efa
Yinka <asyinka@gmail.com>
2023-04-07 09:42:37 +0100

    adding click events to components

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Hello, Ninjas", e);
+  };
+
+  function handleClickAgain(name, e) {
+    console.log(`Hello ${name}`, e.view.alert("Welcome to React, Ninja"));
+  }
+
   return (
     <div>
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain("Tate", e)}>
+        Click another time
+      </button>
     </div>
   );
 };
