fb94ec26d31a444f36698d5ac069b057f88924fa
Yinka <asyinka@gmail.com>
2023-04-07 17:55:01 +0100

    output a list of sample blogs

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,35 @@
 import { useState } from "react";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "Welcome page",
+      body: "lorem ipsolum do...",
+      author: "Patrick",
+      id: 1,
+    },
+    {
+      title: "Lists component in React",
+      body: "lorem ipsolum do...",
+      author: "Shakespeare",
+      id: 2,
+    },
+    {
+      title: "Presidential party",
+      body: "lorem ipsolum do...",
+      author: "Jane",
+      id: 3,
+    },
+  ]);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p> Written by: {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
