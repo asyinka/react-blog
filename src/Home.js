3047b147685df08887aa51c31aa588f6eb66a1db
Yinka <asyinka@gmail.com>
2023-04-07 13:31:28 +0100

    add class to Homepage

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,9 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("Blogger");
-  const [clicks, setClicks] = useState(288);
-
-  const handleClick = () => {
-    setName("John");
-    setClicks(clicks + 1);
-  };
-
   return (
-    <div>
+    <div className="home">
       <h2>Homepage</h2>
-      <p>Welcome {name} </p>
-      <p>This page has {clicks} clicks</p>
-      <button onClick={handleClick}>Click me</button>
     </div>
   );
 };
