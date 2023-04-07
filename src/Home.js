3084faf28aa955267180ea9a27cdfe6655ae63a2
Yinka <asyinka@gmail.com>
2023-04-07 11:29:20 +0100

    add useState

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,20 @@
-const Home = () => {
-  const handleClick = (e) => {
-    console.log("Hello, Ninjas", e);
-  };
+import { useState } from "react";
 
-  function handleClickAgain(name, e) {
-    console.log(`Hello ${name}`, e.view.alert("Welcome to React, Ninja"));
-  }
+const Home = () => {
+  const [name, setName] = useState("Blogger");
+  const [clicks, setClicks] = useState(288);
+
+  const handleClick = () => {
+    setName("John");
+    setClicks(clicks + 1);
+  };
 
   return (
     <div>
       <h2>Homepage</h2>
+      <p>Welcome {name} </p>
+      <p>This page has {clicks} clicks</p>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain("Tate", e)}>
-        Click another time
-      </button>
     </div>
   );
 };
