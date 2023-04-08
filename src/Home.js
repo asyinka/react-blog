abeea4734cbbc7b936e41b5fc625219cdc0cc993
Yinka <asyinka@gmail.com>
2023-04-08 16:05:07 +0100

    apply useEffect to fetch data from rest API

--- src/Home.js
+++ src/Home.js
@@ -2,34 +2,7 @@ import { useState, useEffect } from "react";
 import BlogList from "./ListComponent";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "Welcome page",
-      body: "lorem ipsolum do...",
-      author: "Patrick",
-      id: 1,
-    },
-    {
-      title: "Lists component in React",
-      body: "lorem ipsolum do...",
-      author: "Shakespeare",
-      id: 2,
-    },
-    {
-      title: "Presidential party",
-      body: "lorem ipsolum do...",
-      author: "Jane",
-      id: 3,
-    },
-    {
-      title: "Talking about React",
-      body: "lorem ipsolum do...",
-      author: "Shakespeare",
-      id: 4,
-    },
-  ]);
-
-  const [clickNo, setClickNo] = useState(0);
+  const [blogs, setBlogs] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -38,22 +11,21 @@ const Home = () => {
   };
 
   useEffect(() => {
-    console.log(
-      "run this everytime a state is changed and page is rendered.",
-      clickNo
-    );
-  }, [clickNo]);
+    fetch("http://localhost:8000/blogs")
+      .then((resp) => resp.json())
+      .then((actRes) => setBlogs(actRes));
+  }, []);
 
   return (
     <div className="home">
-      <BlogList
-        blogs={blogs}
-        title="ALL BLOGS"
-        year={2022}
-        handleDelete={handleDelete}
-      />
-      <button onClick={() => setClickNo(clickNo + 1)}>Click-Me</button>
-      <p>You have clicked {clickNo} times </p>
+      {blogs && (
+        <BlogList
+          blogs={blogs}
+          title="ALL BLOGS"
+          year={2022}
+          handleDelete={handleDelete}
+        />
+      )}
     </div>
   );
 };
