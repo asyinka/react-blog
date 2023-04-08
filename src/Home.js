2751a0bbe084552f277f2b2c6bee0ae0d0a0a8eb
Yinka <asyinka@gmail.com>
2023-04-08 16:39:59 +0100

    add message while fetching data

--- src/Home.js
+++ src/Home.js
@@ -4,14 +4,24 @@ import BlogList from "./ListComponent";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
 
+  const [isPending, setIsPending] = useState(true);
+
   useEffect(() => {
+    // setTimeout(() => {
+    //   fetch("http://localhost:8000/blogs")
+    //     .then((resp) => resp.json())
+    //     .then((actResp) => setBlogs(actResp));
+    //   setIsPending(false);
+    // }, 2000);
     fetch("http://localhost:8000/blogs")
       .then((resp) => resp.json())
-      .then((actRes) => setBlogs(actRes));
+      .then((actResp) => setBlogs(actResp));
+    setIsPending(false);
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div>fetching data...</div>}
       {blogs && <BlogList blogs={blogs} title="ALL BLOGS" year={2022} />}
     </div>
   );
